e232b39ce5f40e30d267d7e91c742755bae94156
Zombie_Striker <dannyboyhn@yahoo.com>
2021-05-31 03:46:40 -0400

    Added ability to load jar files

--- src/main/java/com/projectomega/main/plugins/PluginManager.java
+++ src/main/java/com/projectomega/main/plugins/PluginManager.java
@@ -4,15 +4,19 @@ import com.projectomega.main.debugging.DebuggingUtil;
 import com.projectomega.main.game.Omega;
 import example.com.testplugin.TestPlugin;
 import org.apache.xbean.finder.ResourceFinder;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.InputStream;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.logging.Level;
 
 public class PluginManager {
 
@@ -43,19 +47,59 @@ public class PluginManager {
     public static void registerPlugins(File pluginFolder) {
         for (File file : pluginFolder.listFiles()) {
             if (file.getName().endsWith(".jar")) {
+                Omega.getLogger().log(Level.INFO,"Found Jar "+file.getName());
+                JarFile jar = null;
+                InputStream stream = null;
                 try {
-                    ResourceFinder resourceFinder = new ResourceFinder("omega.yml", file.toURL());
-                    List<Class> implem = resourceFinder.findAllImplementations(OmegaPlugin.class);
-                    for (Class c : implem) {
-                        registerPluginClass(c);
+
+                        jar = new JarFile(file);
+                        JarEntry entry = jar.getJarEntry("omega.yml");
+
+                        if (entry == null) {
+                            Omega.getLogger().log(Level.INFO,"Could not find omega.yml");
+                            continue;
+                        }
+
+                        stream = jar.getInputStream(entry);
+
+                        Yaml yaml = new Yaml();
+                        HashMap<String,Object> map = yaml.load(stream);
+
+                        if(!map.containsKey("main")){
+                            Omega.getLogger().log(Level.INFO,"Could not find main entry in omega.yml");
+                            continue;
+                        }
+
+                    ResourceFinder resourceFinder = new ResourceFinder((String) map.get("main"),file.toURL());
+                    //List<Class> implem = resourceFinder.findAllImplementations(OmegaPlugin.class);
+                    try {
+                        URLClassLoader classloader = URLClassLoader.newInstance(new URL[]{file.toURI().toURL()}, PluginManager.class.getClassLoader());
+                        Class<?> jarclass = Class.forName((String) map.get("main"),true,classloader);
+                        Class<? extends  OmegaPlugin> omegaplugin = jarclass.asSubclass(OmegaPlugin.class);
+                        Omega.getLogger().log(Level.INFO,"Registering Plugin Class: -"+ omegaplugin.getName());
+                        registerPluginClass(omegaplugin);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
                     }
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                } finally {
+                    if (jar != null) {
+                        try {
+                            jar.close();
+                        } catch (IOException e) {
+                        }
+                    }
+                    if (stream != null) {
+                        try {
+                            stream.close();
+                        } catch (IOException e) {
+                        }
+                    }
                 }
+
                 //JarFile jar = new JarFile(file);
             }
         }
@@ -63,6 +107,7 @@ public class PluginManager {
 
     public static void enableAllPlugins() {
         for (OmegaPlugin plugin : plugins) {
+            Omega.getLogger().log(Level.INFO,"Enabling "+plugin.getName()+":"+plugin.getVersion());
             plugin.onEnable();
         }
     }
